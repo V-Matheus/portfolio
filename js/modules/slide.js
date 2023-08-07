a788c2350a70701c41a492113075089f49ebff90
Victor Matheus <victormatheus507@gmail.com>
2023-08-07 11:00:22 -0300

    ajustando o posicionamento no arrowNav da sessão #projetos

--- js/modules/slide.js
+++ js/modules/slide.js
@@ -1,16 +1,16 @@
-import debounce from './debounce.js';
+import debounce from "./debounce.js";
 
 export class Slide {
   constructor(slide, wrapper) {
     this.slide = document.querySelector(slide);
     this.wrapper = document.querySelector(wrapper);
     this.dist = { finalPosition: 0, startX: 0, movement: 0 };
-    this.activeClass = 'active';
-    this.changeEvent = new Event('changeEvent');
+    this.activeClass = "active";
+    this.changeEvent = new Event("changeEvent");
   }
 
   transition(active) {
-    this.slide.style.transition = active ? 'transform .3s' : '';
+    this.slide.style.transition = active ? "transform .3s" : "";
   }
 
   moveSlide(distX) {
@@ -25,13 +25,13 @@ export class Slide {
 
   onStart(event) {
     let movetype;
-    if (event.type === 'mousedown') {
+    if (event.type === "mousedown") {
       event.preventDefault();
       this.dist.startX = event.clientX;
-      movetype = 'mousemove';
+      movetype = "mousemove";
     } else {
       this.dist.startX = event.changedTouches[0].clientX;
-      movetype = 'touchmove';
+      movetype = "touchmove";
     }
     this.wrapper.addEventListener(movetype, this.onMove);
     this.transition(false);
@@ -39,7 +39,7 @@ export class Slide {
 
   onMove(event) {
     const pointerPosition =
-      event.type === 'mousemove'
+      event.type === "mousemove"
         ? event.clientX
         : event.changedTouches[0].clientX;
     const finalPosition = this.updatePosition(pointerPosition);
@@ -47,7 +47,7 @@ export class Slide {
   }
 
   onEnd(event) {
-    const movetype = event.type === 'mouseup' ? 'mousemove' : 'touchmove';
+    const movetype = event.type === "mouseup" ? "mousemove" : "touchmove";
     this.wrapper.removeEventListener(movetype, this.onMove);
     this.dist.finalPosition = this.dist.movePosition;
     this.transition(true);
@@ -65,10 +65,10 @@ export class Slide {
   }
 
   addSlideEvents() {
-    this.wrapper.addEventListener('mousedown', this.onStart);
-    this.wrapper.addEventListener('touchstart', this.onStart);
-    this.wrapper.addEventListener('mouseup', this.onEnd);
-    this.wrapper.addEventListener('touchend', this.onEnd);
+    this.wrapper.addEventListener("mousedown", this.onStart);
+    this.wrapper.addEventListener("touchstart", this.onStart);
+    this.wrapper.addEventListener("mouseup", this.onEnd);
+    this.wrapper.addEventListener("touchend", this.onEnd);
   }
 
   // Slides config
@@ -130,7 +130,7 @@ export class Slide {
   }
 
   addResizeEvent() {
-    window.addEventListener('resize', this.onResize);
+    window.addEventListener("resize", this.onResize);
   }
 
   bindEvent() {
@@ -168,28 +168,31 @@ export default class SlideNav extends Slide {
   }
 
   addArrowEvent() {
-    this.prevElement.addEventListener('click', this.activePrevSlide);
-    this.nextElement.addEventListener('click', this.activeNextSlide);
+    this.prevElement.addEventListener("click", this.activePrevSlide);
+    this.nextElement.addEventListener("click", this.activeNextSlide);
   }
 
   createControl() {
-    const control = document.createElement('ul');
-    const arrowNav = document.querySelector('.arrow-nav')
-    control.dataset.control = 'slide';
+    const control = document.createElement("ul");
+    const arrowNav = document.querySelector(".arrow-nav");
+    control.dataset.control = "slide";
 
     this.slideArray.forEach((item, index) => {
       control.innerHTML += `<li><a href='#slide${index + 1}'></a></li>`;
     });
-    arrowNav.appendChild(control);
+
+    const nextButton = document.querySelector(".next");
+    nextButton.parentNode.insertBefore(control, nextButton);
+
     return control;
   }
 
   eventControl(item, index) {
-    item.addEventListener('click', (event) => {
+    item.addEventListener("click", (event) => {
       event.preventDefault();
       this.changeSlide(index);
     });
-    this.wrapper.addEventListener('changeEvent', this.activeControlItem);
+    this.wrapper.addEventListener("changeEvent", this.activeControlItem);
   }
 
   activeControlItem() {
