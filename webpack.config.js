ce18e1226ce0d2862fb3c8386eb165206aa43675
Victor Matheus <victormatheus507@gmail.com>
2023-08-30 22:06:18 -0300

    arquivo .min do js com webpack e babel

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path');
+
+module.exports = {
+  entry: './js/script.js',
+  output: {
+    filename: 'bundle.min.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+    ],
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'dist'), // Diretório base para servir arquivos estáticos
+    },
+    compress: true,
+    port: 8080,
+  },
+};
